48ab973200fbe72f1d15d7444c7440bc41e1de7b
Valentiina VP <34555644+vplentinax@users.noreply.github.com>
2021-01-11 19:25:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const { hash } = window.location
+const {hash} = window.location
 const message = hash.replace('#', '')
 
 if (message) {
-	document.querySelector('#message-form').classList.add('hide')
-	document.querySelector('#message-show').classList.remove('hide')
-	const decode = atob(message)
-	document.querySelector('.message').innerHTML = `${decode}`
+  document.querySelector('#message-form').classList.add('hide')
+  document.querySelector('#message-show').classList.remove('hide')
+  const decode = atob(message)
+  document.querySelector('.message').innerHTML = `${decode}`
 }
 
 document.querySelector('form').addEventListener('submit', event => {
-	event.preventDefault()
-	document.querySelector('#message-form').classList.add('hide')
-	document.querySelector('#link-form').classList.remove('hide')
-	const input = document.querySelector('#message-input')
-	const codify = btoa(input.value)
-	const inputCodify = document.querySelector('#link-input')
-	inputCodify.value = `${window.location}#${codify}`
-	inputCodify.select()
+  event.preventDefault()
+  document.querySelector('#message-form').classList.add('hide')
+  document.querySelector('#link-form').classList.remove('hide')
+  const input = document.querySelector('#message-input')
+  const codify = btoa(input.value)
+  const inputCodify = document.querySelector('#link-input')
+  inputCodify.value = `${window.location}#${codify}`
+  inputCodify.select()
 })
